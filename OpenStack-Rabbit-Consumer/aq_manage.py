3a3e313ff4f1fa0a8e5f89c92a5c5c59567f9137
l-mas <lukas.masaitis@stfc.ac.uk>
2016-11-30 15:35:43 +0000

    RabbitMQ consumer
    Added my work on it so far

---
+++ OpenStack-Rabbit-Consumer/aq_manage.py
@@ -0,0 +1,113 @@
+import subprocess, requests, json
+from requests_kerberos import HTTPKerberosAuth
+
+hostname_url = "http://aquilon.gridpp.rl.ac.uk:6901/find/host?ip={0}"
+
+aq_url_prefix = "https://aquilon.gridpp.rl.ac.uk/private/aqd.cgi"
+aq_make = "/host/{0}/command/make"
+aq_manage = "/host/{0}/command/manage?hostname={0}&{1}={2}&force=true"
+make_reset = "?personality=nubesvms&osversion=6x-x86_64&archetype=ral-tier1&osname=sl"
+
+req_ses = requests.Session()
+req_ses.verify = "/etc/grid-security/certificates/"
+
+def fix_json(fake_json):
+    if isinstance(fake_json, str): fake_json = json.loads(fake_json)
+    out = {}
+    for i in fake_json: out[i["Field"]] = i["Value"]
+    return out
+
+def attempt_url(url, retry=5):
+    auth = HTTPKerberosAuth()
+    out = req_ses.post(url, auth=auth).text
+    #if out == "No data" or out == "":
+        #subprocess.call(["kinit","-k"])
+        #auth = HTTPKerberosAuth()
+    print(out)
+    count = 0
+    while out != "No data" and out != "" and count < retry:
+        out = req_ses.post(url, auth=auth).text
+        print(out)
+        count += 1
+    if count < retry: print("VM: - VM Creation - AQ Sandbox/Domain Assigned")
+    else: print("VM: - VM Creation - AQ Sandbox/Domain Failed")
+
+def get_address_dict(addresses):
+    if not "=" in addresses: return {}
+    addresses = addresses.replace(" ","")
+    addresses = addresses.split(";")
+    d = {}
+    for i in addresses:
+        i = i.split("=")
+        d[i[0]] = i[1].split(",")
+    return d
+
+def get_hostname(payload):
+    if isinstance(payload, str): payload = json.loads(payload)
+    if payload.get("fixed_ips"):
+        ips = payload["fixed_ips"]
+        for i in ips:
+            if i["label"] == "public":
+                ip = i["address"]
+                hostname = req_ses.get(hostname_url.format(ip)).text
+                if hostname:
+                    return hostname
+    if payload.get("instance_id"):
+        iid = payload["instance_id"]
+        vm_info = subprocess.Popen(["openstack","server","show",iid,"-fjson"], stdout=subprocess.PIPE).communicate()[0]
+        if vm_info == "":
+            return None
+        vm_info = fix_json(vm_info)
+        addresses = get_address_dict(vm_info["addresses"])
+        if addresses.get("public"):
+            ip = addresses["public"][0]
+            hostname = req_ses.get(hostname_url.format(ip)).text
+            if hostname:
+                return hostname
+    return payload["hostname"]
+
+def vm_delete(payload):
+    hostname = get_hostname(payload)
+    if hostname == None:
+        print("Hostname can't be found")
+        return
+    aq_url = aq_url_prefix + aq_manage.format(hostname, "domain", "prod")
+    print(aq_url)
+    attempt_url(aq_url)
+    aq_url = aq_url_prefix + aq_make.format(hostname) + make_reset
+    print(aq_url)
+    attempt_url(aq_url)
+
+
+def vm_create(payload):
+    if payload["metadata"]:
+        metadata = payload["metadata"]
+        hostname = get_hostname(payload)
+        if hostname == None:
+            print("Hostname can't be found")
+            return
+
+        if metadata.get("AQ_SANDBOX") or metadata.get("AQ_DOMAIN"):
+            if metadata.get("AQ_SANDBOX"):
+                aq_url = aq_url_prefix + aq_manage.format(hostname, "sandbox", metadata["AQ_SANDBOX"])
+            else:
+                aq_url = aq_url_prefix + aq_manage.format(hostname, "domain", metadata["AQ_DOMAIN"])
+            print(aq_url)
+            attempt_url(aq_url)
+
+        make_suffix = []
+        if metadata.get("AQ_ARCHETYPE"):
+            make_suffix.append("archetype=" + metadata["AQ_ARCHETYPE"])
+        if metadata.get("AQ_PERSONALITY"):
+            make_suffix.append("personality=" + metadata["AQ_PERSONALITY"])
+        if metadata.get("AQ_OS"):
+            make_suffix.append("osname=" + metadata["AQ_OS"])
+        if metadata.get("AQ_OSVERSION"):
+            make_suffix.append("osversion=" + metadata["AQ_OSVERSION"])
+        if make_suffix:
+            make_url_suffix = "?" + "&".join(make_suffix)
+            aq_url = aq_url_prefix + aq_make.format(hostname) + make_url_suffix
+            print(aq_url)
+            attempt_url(aq_url)
+    else:
+        print("No metadata, skipping")
---
+++ OpenStack-Rabbit-Consumer/consumer.py
@@ -0,0 +1,50 @@
+import pika, json, aq_manage, sys
+from ConfigParser import SafeConfigParser
+
+try:
+    config = SafeConfigParser()
+    config.read("consumer.cfg")
+    host = config.get("consumer", "host")
+    login_user = config.get("consumer", "login_user")
+    login_pass = config.get("consumer", "login_pass")
+    exchanges = config.get("consumer", "host")
+    exchanges = exchanges.split("\n")
+except:
+    print("Could not load config file.")
+    sys.exit()
+
+def on_message(channel, method, header, raw_body):
+    body = raw_body.decode("utf-8")
+    message = json.loads(json.loads(body)["oslo.message"])
+    event = message.get("event_type")
+    if event != "compute.metrics.update" and event != "compute.instance.exists":
+        print(event)
+    if event == "compute.instance.create.end":
+        payload = message["payload"]
+        aq_manage.vm_create(payload)
+    elif event == "compute.instance.soft_delete.end":
+        payload = message["payload"]
+        aq_manage.vm_delete(payload)
+    channel.basic_ack(delivery_tag=method.delivery_tag)
+
+credentials = pika.PlainCredentials(login_user,login_pass)
+parameters = pika.ConnectionParameters(host,
+                                       5672,
+                                       "/",
+                                       credentials,
+                                       connection_attempts=10,
+                                       retry_delay=2)
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+channel.queue_declare("ral.info")
+for exchange in exchanges:
+    channel.queue_bind("ral.info",
+                       exchange,
+                       "ral.info")
+channel.basic_consume(on_message, "ral.info")
+
+try:
+    channel.start_consuming()
+except KeyboardInterrupt:
+    channel.stop_consuming()
+connection.close()
