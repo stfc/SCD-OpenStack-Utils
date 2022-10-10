a9d64e0838558560b294ce01aa3146a2e582bba5
apdibbo@clearskies.it <apdibbo@clearskies.it>
2022-10-10 11:17:35 +0100

    Add scripts for generating accounting information

---
+++ OpenStack-nova-accounting/past-accounting.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# helper script for regenerating daily accounting over a period of time
+
+startdate=$1
+enddate=$2
+enddateepoch=`date -d "$enddate" +%s`
+startdateepoch=`date -d "$startdate" +%s`
+
+endtime=$startdate
+starttime=`date -d "$startdate 1 day" +"%Y-%m-%d %H:%M"`
+starttimeepoch=`date -d "$starttime" +%s`
+endtimeepoch=`date -d "$starttime" +%s`
+echo $endtimeepoch
+echo $enddateepoch
+
+
+while [ $endtimeepoch -lt $enddateepoch ];
+do
+    starttime=`date -d "$starttime" +"%Y-%m-%d %H:%M"`
+    starttimeepoch=`date -d "$starttime" +%s`
+    endtime=`date -d "$starttime 1 day" +"%Y-%m-%d %H:%M"`
+    endtimeepoch=`date -d "$endtime" +%s`
+    echo $starttime
+    echo $endtime
+    /usr/local/bin/extract_accounting.py "$starttime" "$endtime"
+    starttime=$endtime
+done;
