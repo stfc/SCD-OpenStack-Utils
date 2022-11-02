3fa651dba096d1f0c11658c4c2c0cd2d86aa3858
David Fairbrother <DavidFair@users.noreply.github.com>
2022-11-02 18:43:03 +0000

    Add tests for aq_api::setup_requests
    Adds various tests to ensure the behaviour of aq_api::setup_requests

--- OpenStack-Rabbit-Consumer/test/test_aq_api.py
+++ OpenStack-Rabbit-Consumer/test/test_aq_api.py
@@ -1,8 +1,10 @@
-from unittest.mock import patch, call
+from unittest import mock
+from unittest.mock import patch, call, NonCallableMock, Mock
 
 import pytest
+from requests.adapters import HTTPAdapter, Retry
 
-from rabbit_consumer.aq_api import verify_kerberos_ticket
+from rabbit_consumer.aq_api import verify_kerberos_ticket, setup_requests
 
 
 def test_verify_kerberos_ticket_valid():
@@ -65,3 +67,73 @@ def test_verify_kerberos_ticket_raises(config, subprocess):
         call(["kinit", "-k"]),
         call(["klist", "-s"]),
     ]
+
+
+@patch("rabbit_consumer.aq_api.requests")
+@patch("rabbit_consumer.aq_api.Retry")
+@patch("rabbit_consumer.aq_api.HTTPAdapter")
+@patch("rabbit_consumer.aq_api.verify_kerberos_ticket")
+def test_setup_requests(verify_kerb, adapter, retry, requests):
+    session = requests.Session.return_value
+    response = session.get.return_value
+    response.status_code = 200
+
+    setup_requests(NonCallableMock(), NonCallableMock(), NonCallableMock())
+    assert session.verify == "/etc/grid-security/certificates/"
+
+    verify_kerb.assert_called_once()
+    retry.assert_called_once_with(total=5, backoff_factor=0.1, status_forcelist=[503])
+    adapter.assert_called_once_with(max_retries=retry.return_value)
+    session.mount.assert_called_once_with("https://", adapter.return_value)
+
+
+@patch("rabbit_consumer.aq_api.requests")
+@patch("rabbit_consumer.aq_api.Retry")
+@patch("rabbit_consumer.aq_api.HTTPAdapter")
+@patch("rabbit_consumer.aq_api.verify_kerberos_ticket")
+def test_setup_requests_throws_for_failed(verify_kerb, adapter, retry, requests):
+    session = requests.Session.return_value
+    response = session.get.return_value
+    response.status_code = 500
+
+    with pytest.raises(Exception):
+        setup_requests(NonCallableMock(), NonCallableMock(), NonCallableMock())
+
+    assert session.verify == "/etc/grid-security/certificates/"
+
+    verify_kerb.assert_called_once()
+    retry.assert_called_once_with(total=5, backoff_factor=0.1, status_forcelist=[503])
+    adapter.assert_called_once_with(max_retries=retry.return_value)
+    session.mount.assert_called_once_with("https://", adapter.return_value)
+    session.get.assert_called_once()
+
+
+@pytest.mark.parametrize("rest_verb", ["get", "post", "put", "delete"])
+@patch("rabbit_consumer.aq_api.requests")
+@patch("rabbit_consumer.aq_api.HTTPKerberosAuth")
+@patch("rabbit_consumer.aq_api.verify_kerberos_ticket")
+def test_setup_requests_rest_methods(_, kerb_auth, requests, rest_verb):
+    url, desc = NonCallableMock(), NonCallableMock()
+
+    session = requests.Session.return_value
+
+    rest_method = getattr(session, rest_verb)
+    response = rest_method.return_value
+    response.status_code = 200
+
+    assert setup_requests(url, rest_verb, desc) == response.text
+    rest_method.assert_called_once_with(url, auth=kerb_auth.return_value)
+
+
+@patch("rabbit_consumer.aq_api.requests")
+@patch("rabbit_consumer.aq_api.HTTPKerberosAuth")
+@patch("rabbit_consumer.aq_api.verify_kerberos_ticket")
+def test_setup_requests_get(_, kerb_auth, requests):
+    url, desc = NonCallableMock(), NonCallableMock()
+    session = requests.Session.return_value
+    response = session.get.return_value
+    response.status_code = 200
+
+    assert setup_requests(url, "get", desc) == response.text
+
+    session.get.assert_called_once_with(url, auth=kerb_auth.return_value)
