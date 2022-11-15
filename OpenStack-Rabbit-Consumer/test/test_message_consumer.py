446f8abc588db7676d2cd4262b8ffcd370759875
David Fairbrother <DavidFair@users.noreply.github.com>
2022-11-15 16:13:09 +0000

    Add Unit tests to create host in Aquillon
    Adds unit tests and reshapes the original code in a way to enable
    testing of the create host path for Aquillon.

--- OpenStack-Rabbit-Consumer/test/test_message_consumer.py
+++ OpenStack-Rabbit-Consumer/test/test_message_consumer.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Union, Dict
 from unittest import mock
-from unittest.mock import Mock, NonCallableMock, patch, call
+from unittest.mock import Mock, NonCallableMock, patch, call, MagicMock
 
 import pytest
 
@@ -9,6 +9,8 @@ from rabbit_consumer.message_consumer import (
     get_metadata_value,
     on_message,
     initiate_consumer,
+    consume,
+    convert_hostnames,
 )
 
 
@@ -97,28 +99,28 @@ def test_on_message_parses_json(json, _):
 
 @patch("rabbit_consumer.message_consumer.consume")
 @patch("rabbit_consumer.message_consumer.json")
-def test_on_message_forwards_json_to_consumer(json, consume):
+def test_on_message_forwards_json_to_consumer(json, consume_mock):
     channel = Mock()
     method = Mock()
     header = Mock()
     raw_body = Mock()
 
     on_message(channel, method, header, raw_body)
-    consume.assert_called_once_with(json.loads.return_value)
+    consume_mock.assert_called_once_with(json.loads.return_value)
     channel.basic_ack.assert_called_once_with(delivery_tag=method.delivery_tag)
 
 
 @patch("rabbit_consumer.message_consumer.consume")
 @patch("rabbit_consumer.message_consumer.json")
-def test_on_message_exception_handling(_, consume):
+def test_on_message_exception_handling(_, consume_mock):
     channel = Mock()
     method = Mock()
     header = Mock()
     raw_body = Mock()
 
-    consume.side_effect = Exception("test")
+    consume_mock.side_effect = Exception("test")
     on_message(channel, method, header, raw_body)
-    consume.assert_called_once()
+    consume_mock.assert_called_once()
     channel.basic_ack.assert_called_once_with(delivery_tag=method.delivery_tag)
 
 
@@ -210,3 +212,306 @@ def test_initiate_consumer_keyboard_interrupt(pika, sys_exit, _):
     connection = pika.BlockingConnection.return_value
     connection.close.assert_called_once()
     sys_exit.assert_called_once_with(0)
+
+
+@patch("rabbit_consumer.message_consumer.socket")
+def test_convert_hostnames_multiple(socket):
+    message = Mock()
+    message.get.return_value = {"fixed_ips": [Mock(), Mock()]}
+    socket.gethostbyaddr.side_effect = [["host1"], ["host2"]]
+
+    hostnames = convert_hostnames(message, "")
+    assert hostnames == ["host1", "host2"]
+
+
+@patch("rabbit_consumer.message_consumer.socket")
+def test_convert_hostnames_single(socket):
+    message = Mock()
+    message.get.return_value = {"fixed_ips": [Mock()]}
+    socket.gethostbyaddr.side_effect = [["host1"]]
+
+    hostnames = convert_hostnames(message, "")
+    assert hostnames == ["host1"]
+
+
+@patch("rabbit_consumer.message_consumer.socket")
+def test_convert_hostnames_no_ips(_):
+    message = Mock()
+    message.get.return_value = {"fixed_ips": []}
+
+    vm_name = "mocked_name"
+    hostnames = convert_hostnames(message, vm_name)
+    assert hostnames == [vm_name + ".novalocal"]
+
+
+_FAKE_PAYLOAD = {
+    "instance_id": NonCallableMock(),
+    "display_name": "vm_display_name",
+    "fixed_ips": [Mock(), Mock()],
+    "metadata": MagicMock(),
+    "host": NonCallableMock(),
+    "vcpus": NonCallableMock(),
+    "memory_mb": NonCallableMock(),
+    "first_ip": [{"address": NonCallableMock()}],
+}
+
+
+def _message_get_create(arg_name: str) -> Union[str, Dict]:
+    if arg_name == "event_type":
+        return "compute.instance.create.end"
+    if arg_name == "payload":
+        return _FAKE_PAYLOAD
+    return arg_name
+
+
+@patch("rabbit_consumer.message_consumer.is_aq_message")
+@patch("rabbit_consumer.message_consumer.get_metadata_value")
+@patch("rabbit_consumer.message_consumer.convert_hostnames")
+@patch("rabbit_consumer.message_consumer.openstack_api")
+@patch("rabbit_consumer.message_consumer.aq_api")
+@patch("rabbit_consumer.message_consumer.RabbitConsumer")
+def test_consume_create_machine_hostnames_good_path(
+    app_conf, aq_api, openstack, hostname, get_metadata, _
+):
+    expected_hostnames = ["host1", "host2"]
+    hostname.return_value = expected_hostnames
+
+    message = Mock()
+    message.get.side_effect = _message_get_create
+
+    consume(message)
+    aq_api.create_machine.assert_called_once_with(
+        _FAKE_PAYLOAD["instance_id"],
+        _FAKE_PAYLOAD["host"],
+        _FAKE_PAYLOAD["vcpus"],
+        _FAKE_PAYLOAD["memory_mb"],
+        expected_hostnames[-1],
+        app_conf.config.get.return_value,
+    )
+
+    aq_api.add_machine_interface.assert_has_calls(
+        [
+            call(
+                aq_api.create_machine.return_value,
+                _FAKE_PAYLOAD["fixed_ips"][0].get.return_value,
+                "eth0",
+            ),
+            call(
+                aq_api.create_machine.return_value,
+                _FAKE_PAYLOAD["fixed_ips"][1].get.return_value,
+                "eth1",
+            ),
+        ]
+    )
+
+    aq_api.update_machine_interface.assert_called_once_with(
+        aq_api.create_machine.return_value, "eth0"
+    )
+
+    aq_api.create_host.assert_called_once_with(
+        expected_hostnames[0],
+        aq_api.create_machine.return_value,
+        get_metadata.return_value,
+        _FAKE_PAYLOAD["fixed_ips"][0].get.return_value,
+        get_metadata.return_value,
+        get_metadata.return_value,
+        get_metadata.return_value,
+        get_metadata.return_value,
+        get_metadata.return_value,
+    )
+
+    openstack.update_metadata.assert_has_calls(
+        [
+            # Where _context_project_id is the arg passed to message.get()
+            call(
+                "_context_project_id",
+                _FAKE_PAYLOAD["instance_id"],
+                {"HOSTNAMES": ", ".join(expected_hostnames)},
+            ),
+            call(
+                "_context_project_id",
+                _FAKE_PAYLOAD["instance_id"],
+                {"AQ_MACHINENAME": aq_api.create_machine.return_value},
+            ),
+            call(
+                "_context_project_id",
+                _FAKE_PAYLOAD["instance_id"],
+                {"AQ_STATUS": "SUCCESS"},
+            ),
+        ],
+        any_order=False,
+    )
+
+    aq_api.aq_manage.assert_has_calls(
+        [
+            call(host, "sandbox", get_metadata.return_value)
+            for host in expected_hostnames
+        ]
+    )
+    aq_api.aq_make.assert_has_calls(
+        [
+            call(
+                host,
+                get_metadata.return_value,
+                get_metadata.return_value,
+                get_metadata.return_value,
+                get_metadata.return_value,
+            )
+            for host in expected_hostnames
+        ]
+    )
+
+
+@patch("rabbit_consumer.message_consumer.RabbitConsumer")
+@patch("rabbit_consumer.message_consumer.is_aq_message")
+@patch("rabbit_consumer.message_consumer.convert_hostnames")
+@patch("rabbit_consumer.message_consumer.openstack_api")
+def test_consume_create_machine_update_metadata_failure(openstack, hostname, _, __):
+    hostname.return_value = ["host1", "host2"]
+
+    message = Mock()
+    message.get.side_effect = _message_get_create
+
+    openstack.update_metadata.side_effect = Exception("mocked exception")
+
+    with pytest.raises(Exception) as err:
+        consume(message)
+    assert str(err.value) == "Failed to update metadata"
+
+
+@patch("rabbit_consumer.message_consumer.is_aq_message")
+@patch("rabbit_consumer.message_consumer.openstack_api")
+@patch("rabbit_consumer.message_consumer.RabbitConsumer")
+@patch("rabbit_consumer.message_consumer.convert_hostnames")
+@patch("rabbit_consumer.message_consumer.aq_api")
+def test_consume_create_machine_aq_api_failure(aq_api, hostname, _, __, ___):
+    hostname.return_value = ["host1", "host2"]
+
+    message = Mock()
+    message.get.side_effect = _message_get_create
+
+    aq_api.create_machine.side_effect = Exception("mocked exception")
+
+    with pytest.raises(Exception) as err:
+        consume(message)
+    assert str(err.value) == "Failed to create machine: mocked exception"
+
+
+@patch("rabbit_consumer.message_consumer.is_aq_message")
+@patch("rabbit_consumer.message_consumer.openstack_api")
+@patch("rabbit_consumer.message_consumer.RabbitConsumer")
+@patch("rabbit_consumer.message_consumer.convert_hostnames")
+@patch("rabbit_consumer.message_consumer.aq_api")
+def test_consume_add_machine_interface_failure(aq_api, hostname, _, __, ___):
+    hostname.return_value = ["host1", "host2"]
+
+    message = Mock()
+    message.get.side_effect = _message_get_create
+
+    aq_api.add_machine_interface.side_effect = Exception("mocked exception")
+
+    with pytest.raises(Exception) as err:
+        consume(message)
+    assert "Failed to add machine interface" in str(err.value)
+    assert "mocked exception" in str(err.value)
+
+
+@patch("rabbit_consumer.message_consumer.is_aq_message")
+@patch("rabbit_consumer.message_consumer.openstack_api")
+@patch("rabbit_consumer.message_consumer.RabbitConsumer")
+@patch("rabbit_consumer.message_consumer.convert_hostnames")
+@patch("rabbit_consumer.message_consumer.aq_api")
+def test_consume_add_machine_interface_address_failure(aq_api, hostname, _, __, ___):
+    hostname.return_value = ["host1", "host2"]
+
+    message = Mock()
+    message.get.side_effect = _message_get_create
+
+    aq_api.add_machine_interface_address.side_effect = Exception("mocked exception")
+
+    with pytest.raises(Exception) as err:
+        consume(message)
+    assert "Failed to add machine interface address" in str(err.value)
+    assert "mocked exception" in str(err.value)
+
+
+@patch("rabbit_consumer.message_consumer.is_aq_message")
+@patch("rabbit_consumer.message_consumer.openstack_api")
+@patch("rabbit_consumer.message_consumer.RabbitConsumer")
+@patch("rabbit_consumer.message_consumer.convert_hostnames")
+@patch("rabbit_consumer.message_consumer.aq_api")
+def test_consume_update_machine_interface_failure(aq_api, hostname, _, __, ___):
+    hostname.return_value = ["host1", "host2"]
+
+    message = Mock()
+    message.get.side_effect = _message_get_create
+
+    aq_api.update_machine_interface.side_effect = Exception("mocked exception")
+
+    with pytest.raises(Exception) as err:
+        consume(message)
+    assert "Failed to set default interface" in str(err.value)
+    assert "mocked exception" in str(err.value)
+
+
+@patch("rabbit_consumer.message_consumer.is_aq_message")
+@patch("rabbit_consumer.message_consumer.openstack_api")
+@patch("rabbit_consumer.message_consumer.RabbitConsumer")
+@patch("rabbit_consumer.message_consumer.convert_hostnames")
+@patch("rabbit_consumer.message_consumer.aq_api")
+def test_consume_create_host_failure(aq_api, hostname, _, __, ___):
+    hostname.return_value = ["host1", "host2"]
+
+    message = Mock()
+    message.get.side_effect = _message_get_create
+
+    vm_name = "vm-openstack-AbC-123"
+    aq_api.create_host.side_effect = Exception(vm_name)
+
+    with pytest.raises(Exception) as err:
+        consume(message)
+
+    assert "IP Address already exists on" in str(err.value)
+    assert vm_name in str(err.value)
+
+
+@patch("rabbit_consumer.message_consumer.is_aq_message")
+@patch("rabbit_consumer.message_consumer.RabbitConsumer")
+@patch("rabbit_consumer.message_consumer.openstack_api")
+@patch("rabbit_consumer.message_consumer.convert_hostnames")
+@patch("rabbit_consumer.message_consumer.aq_api")
+def test_aq_manage_failure_marks_aq_failed(aq_api, hostname, openstack, __, ___):
+    hostname.return_value = ["host1", "host2"]
+
+    message = Mock()
+    message.get.side_effect = _message_get_create
+    aq_api.aq_manage.side_effect = Exception("mocked_exception")
+
+    with pytest.raises(Exception) as err:
+        consume(message)
+
+    openstack.update_metadata.assert_called_with(
+        "_context_project_id", _FAKE_PAYLOAD["instance_id"], {"AQ_STATUS": "FAILED"}
+    )
+    assert "Failed to set Aquilon configuration" in str(err.value)
+
+
+@patch("rabbit_consumer.message_consumer.is_aq_message")
+@patch("rabbit_consumer.message_consumer.RabbitConsumer")
+@patch("rabbit_consumer.message_consumer.openstack_api")
+@patch("rabbit_consumer.message_consumer.convert_hostnames")
+@patch("rabbit_consumer.message_consumer.aq_api")
+def test_aq_make_failure_marks_aq_failed(aq_api, hostname, openstack, __, ___):
+    hostname.return_value = ["host1", "host2"]
+
+    message = Mock()
+    message.get.side_effect = _message_get_create
+    aq_api.aq_make.side_effect = Exception("mocked_exception")
+
+    with pytest.raises(Exception) as err:
+        consume(message)
+
+    openstack.update_metadata.assert_called_with(
+        "_context_project_id", _FAKE_PAYLOAD["instance_id"], {"AQ_STATUS": "FAILED"}
+    )
+    assert "Failed to set Aquilon configuration" in str(err.value)
