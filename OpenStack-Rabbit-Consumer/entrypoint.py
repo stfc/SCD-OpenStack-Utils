a41172f0bcfa6b3ab9bcd368d7fe546015efc14b
David Fairbrother <DavidFair@users.noreply.github.com>
2022-11-22 17:25:32 +0000

    Add sigint handler to get out of inf loop
    Adds a sigint handler to dynamically drop us out of the infinite loop
    without losing all of our current work.

--- OpenStack-Rabbit-Consumer/entrypoint.py
+++ OpenStack-Rabbit-Consumer/entrypoint.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
 
 import logging
+import signal
 import sys
 
 from rabbit_consumer.rabbit_consumer import ConsumerLoop
+
+
+def _sigint_handler(*_):
+    """
+    Handles the SIGINT signal
+    """
+    logging.info("SIGINT received, shutting down")
+    ConsumerLoop.state = ConsumerLoop.state.STOP
+
 
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     logging.getLogger("pika").setLevel(logging.ERROR)
     logging.getLogger("requests").setLevel(logging.WARNING)
     logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+    signal.signal(signal.SIGINT, _sigint_handler)
 
     consumer = ConsumerLoop()
     consumer.start()
