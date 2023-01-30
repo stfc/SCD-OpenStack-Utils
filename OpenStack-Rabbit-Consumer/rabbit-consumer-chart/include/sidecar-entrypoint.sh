c7e699ba0bac4c339879ac09651f0c82fd5f47df
David Fairbrother <DavidFair@users.noreply.github.com>
2023-01-30 17:26:27 +0000

    Complete Kerb sidecar for dev env
    Completes and tests the krb5 auth flow using the sidecar against dev
    credentials

--- OpenStack-Rabbit-Consumer/rabbit-consumer-chart/include/sidecar-entrypoint.sh
+++ OpenStack-Rabbit-Consumer/rabbit-consumer-chart/include/sidecar-entrypoint.sh
@@ -4,7 +4,15 @@ set -ex
 
 dnf install -y krb5-workstation
 
-cat /etc/krb5.conf
-mkdir /shared/krb5cc
+echo "Principle set to: $KRB5_PRINCIPLE"
 
+useradd -r krb5user
+# Run kinit.sh as system user to reduce risk of privilege escalation
+su --preserve-environment -c '/etc/entrypoints.d/kinit.sh' krb5user
+
+# We should never get here
+echo "ERROR: Exited while loop"
 sleep infinity
+
+#TODO
+# exit 1
