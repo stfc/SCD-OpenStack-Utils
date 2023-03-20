39b17277061293efc5105519486c364450ea67f5
anish.mudaraddi <anish.mudaraddi@gmail.com>
2023-03-20 19:31:49 +0000

    refactored some functions, added requirements.txt

--- cloud-energy-collection/iriscasttools/utils.py
+++ cloud-energy-collection/iriscasttools/utils.py
@@ -2,24 +2,35 @@ import subprocess
 import time
 import os
 import psutil
+from typing import Dict
+
+def retry(func, retry_on: (Exception,), retries: int = 3, delay: int = 3, backoff: int = 2):    
+    """A retry function """
+   
+    for i in range(retries + 1):
+        try:
+            return func()
+        except retry_on:
+            if i == retries:
+                return None
+        seconds = delay + (backoff * i)
+        time.sleep(seconds)
+    return wrapper
 
 
-def run_cmd(cmd_args, max_retries=3):
+def run_cmd(cmd_args: str): 
     """ Run command with given arguments and return output """
-    for retry in range(max_retries):
-        res_list = subprocess.Popen(cmd_args, 
-                                    shell=True, 
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE
-                                ).communicate()
-        if res_list[1]:
-            # encountered a failure. Node may be busy
-            # Wait for longer time after each consecutive failure
-            time.sleep(retry+1)
-        else:
-            return res_list[0]
+    res_list = subprocess.Popen(
+        cmd_args, 
+        shell=True, 
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    ).communicate()
 
-    return None
+    # assert we did not find any errors
+    assert not res_list[1]  
+    return res_list[0]
+
 
 def check_ipmi_conn():
     """ Check if IPMItool exists and can be connected to """ 
@@ -30,10 +41,19 @@ def check_ipmi_conn():
 
 def ipmi_raw_power_query():
     """ Get raw power query from IPMItool """
-    return run_cmd("/usr/sbin/ipmi-dcmi --get-system-power-statistics")
+    try:
+        return retry(
+            lambda: run_cmd("/usr/sbin/ipmi-dcmi --get-system-power-statistics"),
+            AssertionError,
+            retries= 3,
+            delay = 3, 
+            backoff = 2,
+        )
+        
+    except AssertionError:
+        return None
 
-
-def to_csv(stats, include_header=False):
+def to_csv(stats: Dict, include_header: bool = False):
     """ convert output to csv """ 
     res = ",".join([str(s) for s in stats.values()])
     if include_header:
@@ -65,7 +85,7 @@ def get_ipmi_power_stats(*args):
 
             if stat in power_stats.keys():
                 power_stats[stat] = re.search("[0-9]+", stat).group(0)
- 
+
     return power_stats
 
 def get_os_load(*args):
@@ -100,10 +120,22 @@ def get_ram_usage(*args):
 
     try:
         stats['max_ram_kb'] = int(
-            run_cmd("free -k | sed -n '2p' | awk '{print $2}'")
+            retry(
+                lambda: run_cmd("free -k | sed -n '2p' | awk '{print $2}'"),
+                retry_on=AssertionError,
+                retries=3,
+                delay=3,
+                backoff=2
+            )
         )
         stats['used_ram_kb'] = int(
-            run_cmd("free -k | sed -n '2p' | awk '{print $2}'")
+            retry(
+                lambda: run_cmd("free -k | sed -n '2p' | awk '{print $2}'"),
+                retry_on=AssertionError,
+                retries=3,
+                delay=3,
+                backoff=2
+            )
         )
 
         if max_ram and used_ram:
@@ -111,7 +143,7 @@ def get_ram_usage(*args):
                 (used_ram / max_ram) * 100, 3
             )
 
-    except ValueError:
+    except (AssertionError, ValueError):
         pass
    
     res.update({k:v for k,v in stats.items() if k in args})
