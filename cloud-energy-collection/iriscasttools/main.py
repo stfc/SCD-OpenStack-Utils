41576399b2707b62e664d4db7f3b3af6ebde08ab
anish.mudaraddi <anish.mudaraddi@gmail.com>
2023-03-23 16:25:12 +0000

    added tests for iriscasttools/main.py
    fixed upbugs and pylint issues in main.py.

--- cloud-energy-collection/iriscasttools/main.py
+++ cloud-energy-collection/iriscasttools/main.py
@@ -1,53 +1,27 @@
 """
 Collects energy usage metrics using IPMI, as well as other metrics such as CPU and RAM usage.
 """
-
-from utils import (
-    to_csv,
-    get_ipmi_power_stats,
-    get_os_load,
-    get_ram_usage,
-)
+import utils
 
 
-def get_iriscast_stats(poll_period_seconds=300, csv=False, include_header=False):
-    """get stats for iriscast"""
+def get_iriscast_stats(csv=False, include_header=False):
+    """
+    Get stats for iriscast
+
+    Keyword arguments:
+        csv -- bool, flag to set if output should be formatted as csv or dict
+        include_header -- bool, flag to set if header should be included if csv flag set
+
+    """
 
     all_stats = {}
-    # get all available power info
-    power_stats = get_ipmi_power_stats(
-        "current_power",
-        "minimum_power_over_sampling_duration",
-        "maximum_power_over_sampling_duration",
-        "average_power_over_sampling_duration",
-        "statistics_reporting_time_period",
-    )
-    power_stats["watt_hours"] = ""
 
-    if power_stats["current_power"] != "":
-        power_stats["watt_hours"] = round(
-            int(power_stats["current_power"]) * (poll_period_seconds / 3600), 3
-        )
+    power_stats = utils.get_ipmi_power_stats("current_power")
 
     all_stats.update(power_stats)
-    all_stats.update(get_os_load("os_load1", "os_load5", "os_load15"))
-    all_stats.update(get_ram_usage("ram_usage_percentage"))
+    all_stats.update(utils.get_os_load("os_load5"))
+    all_stats.update(utils.get_ram_usage("ram_usage_percentage"))
 
     if csv:
-        return to_csv(all_stats, include_header)
-    return all_stats
-
-
-def get_cloud_monitoring_stats(csv=False, include_header=False):
-    """get stats for cloud monitoring"""
-    all_stats = {}
-
-    power_stats = get_ipmi_power_stats("current_power")
-
-    all_stats.update(power_stats)
-    all_stats.update(get_os_load("os_load5"))
-    all_stats.update(get_ram_usage("ram_usage_percentage"))
-
-    if csv:
-        return to_csv(all_stats, include_header)
+        return utils.to_csv(all_stats, include_header)
     return all_stats
---
+++ cloud-energy-collection/iriscasttools/test/test_main.py
@@ -0,0 +1,56 @@
+"""
+Tests for main functions for iriscasttools package
+"""
+
+from unittest.mock import patch
+import pytest
+
+from main import get_iriscast_stats
+
+
+@pytest.mark.parametrize(
+    "test_csv_flag, test_include_header",
+    [(False, False), (True, True), (True, False)],
+)
+@patch("utils.get_ipmi_power_stats")
+@patch("utils.get_os_load")
+@patch("utils.get_ram_usage")
+@patch("utils.to_csv")
+def test_get_iriscast_stats(
+    mock_to_csv,
+    mock_get_ram_usage,
+    mock_get_os_load,
+    mock_get_ipmi_power_stats,
+    test_csv_flag,
+    test_include_header,
+):
+    """
+    Test get iriscast stats function
+
+    Keyword arguments:
+        - mock_get_ipmi_power_stats -- Mock obj for get_ipmi_power_stats
+        - test_poll_period -- int, intervals between each reading, used to calculate watt hours
+    """
+
+    expected_power_stats = {"current_power": ""}
+    expected_os_load_stats = {"os_load5": ""}
+    expected_ram_stats = {"ram_usage_percentage": ""}
+
+    expected_all = dict(expected_power_stats)
+    expected_all.update(expected_os_load_stats)
+    expected_all.update(expected_ram_stats)
+
+    mock_get_ipmi_power_stats.side_effect = [expected_power_stats]
+    mock_get_os_load.return_value = expected_os_load_stats
+    mock_get_ram_usage.return_value = expected_ram_stats
+
+    _ = get_iriscast_stats(test_csv_flag, test_include_header)
+
+    mock_get_ipmi_power_stats.assert_called_once_with("current_power")
+    mock_get_os_load.assert_called_once_with("os_load5")
+    mock_get_ram_usage.assert_called_once_with("ram_usage_percentage")
+
+    if test_csv_flag:
+        mock_to_csv.assert_called_once_with(expected_all, test_include_header)
+    else:
+        assert not mock_to_csv.called
