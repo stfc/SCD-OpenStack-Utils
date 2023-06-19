fb3aa7eb10346c673ed394cf3aac024512486bb1
charlieDurnsfordHollands <charlie.durnsford-hollands@stfc.ac.uk>
2023-06-19 08:51:04 +0100

    Make suggested changes

--- aq_zombie_finder/aq_zombie_finder.py
+++ aq_zombie_finder/aq_zombie_finder.py
@@ -1,31 +1,45 @@
+import argparse
+import os
 import paramiko
 from re import compile
-from getpass import getpass
 
 
-# Creates an SSH client through Paramiko with the users details and returns the client
 def create_client(host, user, password):
+    """
+    Function to create an SSH client through Paramiko
+        :param host: hostname to connect to (string)
+        :param user: user to connect as (string)
+        :param password: password for the user (string)
+        :returns:  Configured Paramiko SSHClient object
+    """
     client = paramiko.client.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
     client.connect(hostname=host, username=user, password=password, timeout=60)
     return client
 
 
-# Executes a command through a Paramiko client, waits for an exit status and outputs the response
 def ssh_command(client, command):
+    """
+    Function to execute a command over an SSH client through Paramiko
+        :param client: Paramiko client (client)
+        :param command: user to connect as (string)
+        :returns:  Console output of the command
+    """
     _, _stdout, _ = client.exec_command(command, get_pty=True, timeout=60)
     _stdout.channel.recv_exit_status()
     return _stdout.readlines()
 
 
-# Return a list of the IPs from AQ manage VMs
 def get_aq_ips(openstack_client):
-    # Run a command on the openstack VM to get a list of
-    # all servers with "-aq" in their details that start with "172.16"
+    """
+    Function to run a command on the openstack VM
+        :param openstack_client: Paramiko client (client)
+        :returns:   List of the IPs of machines using aq images from machines with IPs starting with "172.16."
+    """
     servers = ssh_command(
         openstack_client,
         "source admin-openrc.sh ; "
-        "openstack server list --all-projects --long --limit -1 --ip=172.16 | "
+        "openstack server list --all-projects --long --limit -1 --ip=172.16. | "
         "grep -i '\\-aq'",
     )
 
@@ -36,24 +50,35 @@ def get_aq_ips(openstack_client):
     return ip_rexp.findall(str(servers))
 
 
-# Check if an AQ host exists for an AQ managed VM and if so, return the host
-def check_openstack_ip(aq_ip, aquilon_client, openstack_zombie_file):
-    # Format the IP address and check the host information of the VM at that IP address in Aquilon
+def check_openstack_ip(aq_ip, aquilon_client, openstack_zombie_filepath):
+    """
+    Function to check the host of an AQ managed machine
+        :param aq_ip: The IP to check (String)
+        :param aquilon_client: Paramiko client (client)
+        :param openstack_zombie_filepath: The filepath of the openstack output file (String)
+        :returns:   The host of the AQ managed VM if it exists, otherwise False
+    """
     ip_string = aq_ip.replace(".", "-")
     ip_string = "host-" + ip_string + ".nubes.stfc.ac.uk"
     aq_host = ssh_command(aquilon_client, "aq show_host --hostname " + ip_string)
-
     if "Not Found:" in str(aq_host):
         # If the VM doesn't have details in Aquilon, save its IP address as a potential zombie
-        openstack_zombie_file.write(ip_string + "\n")
+        with open(openstack_zombie_filepath, 'a') as openstack_zombie_file:
+            openstack_zombie_file.write(ip_string + "\n")
         return False
 
     else:
         return aq_host
 
 
-# Check if the serial number matches the host
-def check_aquilon_serial(aq_host, aq_ip, openstack_client, aquilon_zombie_file):
+def check_aquilon_serial(aq_host, aq_ip, openstack_client, aquilon_zombie_filepath):
+    """
+    Function to check if a serial number of a VM matches that of the AQ host
+        :param aq_host: The host of the VM (String)
+        :param aq_ip: The IP to check (String)
+        :param openstack_client: Paramiko client (client)
+        :param aquilon_zombie_filepath: The filepath of the Aquilon output file (String)
+    """
     # Define the Regex expression used to find the Serial number
     serial_rexp = compile(r"((?<=Serial: )(.+?)(?=\\r))")
 
@@ -67,24 +92,45 @@ def check_aquilon_serial(aq_host, aq_ip, openstack_client, aquilon_zombie_file):
 
     if "No server with a name or ID of" in str(server_details):
         # If the serial doesn't return a host, save its serial and IP as a potential zombie
-        aquilon_zombie_file.write(serial + " | " + aq_ip + "\n")
+        with open(aquilon_zombie_filepath, 'a') as aquilon_zombie_file:
+            aquilon_zombie_file.write(serial + " | " + aq_ip + "\n")
+
+
+def check_output_files(openstack_zombie_filepath, aquilon_zombie_filepath):
+    """
+    Function to check if output files already exist
+        :param aquilon_zombie_filepath: The filepath of the Aquilon output file (String)
+        :param openstack_zombie_filepath: The filepath of the Openstack output file (String)
+    """
+    if os.path.exists(openstack_zombie_filepath):
+        raise RuntimeError(f"{openstack_zombie_filepath} already exists")
+    if os.path.exists(aquilon_zombie_filepath):
+        raise RuntimeError(f"{aquilon_zombie_filepath} already exists")
 
 
 def aq_zombie_finder():
-    # Prompt the user for their FedID, Password and Openstack IP to connect to
-    user = input("FedID: ")
-    password = getpass("Password: ")
-    openstack_ip = input("Openstack IP: ")
+    # Get arguments passed to the script
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("-u", "--user", metavar="USER", help="FedID of the user", required=True)
+    parser.add_argument("-p", "--password", metavar="PASSWORD", help="Password of the user", required=True)
+    parser.add_argument("-i", "--ip", metavar="IP", help="IP of the machine with Openstack to SSH to", required=True)
+    parser.add_argument("-o", "--output", metavar="OUTPUT", help="Directory to create the output files in")
+
+    # Define the variables with the script arguments
+    args = parser.parse_args()
+    user = args.user
+    password = args.password
+    openstack_ip = args.ip
+    output = args.output
 
     # Create a paramiko SSH client to the VM running Openstack and to Aquilon
     openstack_client = create_client(openstack_ip, user, password)
     aquilon_client = create_client("aquilon.gridpp.rl.ac.uk", user, password)
 
-    # Create a blank text documents for the output to be saved to
-    open("output\\openstack_zombie_list.txt", "w").close()
-    open("output\\aquilon_zombie_list.txt", "w").close()
-    openstack_zombie_file = open("output\\openstack_zombie_list.txt", "a")
-    aquilon_zombie_file = open("output\\aquilon_zombie_list.txt", "a")
+    # Define the filepath for the output to be saved to
+    openstack_zombie_filepath = os.path.join(output or "output", "openstack_zombie_list.txt")
+    aquilon_zombie_filepath = os.path.join(output or "output", "aquilon_zombie_list.txt")
+    check_output_files(openstack_zombie_filepath, aquilon_zombie_filepath)
 
     # Get a list of the IPs of AQ VMs
     aq_ips = get_aq_ips(openstack_client)
@@ -95,22 +141,18 @@ def aq_zombie_finder():
         print(i, "/", len(aq_ips))
 
         # Check the ip of the Aquilon VM
-        aq_host = check_openstack_ip(aq_ip[0], aquilon_client, openstack_zombie_file)
+        aq_host = check_openstack_ip(aq_ip[0], aquilon_client, openstack_zombie_filepath)
 
         # Check if an aquilon host exists
         if aq_host:
             # Check the serial number of the AQ host
             check_aquilon_serial(
-                aq_host, aq_ip[0], openstack_client, aquilon_zombie_file
+                aq_host, aq_ip[0], openstack_client, aquilon_zombie_filepath
             )
 
     # Close the SSH clients
     openstack_client.close()
     aquilon_client.close()
-
-    # Close the text files
-    openstack_zombie_file.close()
-    aquilon_zombie_file.close()
 
 
 if __name__ == "__main__":
