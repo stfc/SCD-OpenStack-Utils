f6da457ed894a5cd1f3c5ed1443647b2955af280
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2023-06-29 09:08:02 +0100

    hotfix - iriscasttools path not set properly changed 'python-version' to 'python_version'

--- iriscasttools/setup.py
+++ iriscasttools/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.2.0"
+VERSION = "0.2.1"
 DESCRIPTION = "python package for IRISCAST tools"
 
 LONG_DESCRIPTION = """Python package to get power and CPU/RAM usage statistics"""
