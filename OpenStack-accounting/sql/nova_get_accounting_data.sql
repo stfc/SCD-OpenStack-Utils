cf15b7406efaa1e283d0f095bd4a472029af764b
apdibbo@clearskies.it <apdibbo@clearskies.it>
2023-10-05 15:29:56 +0100

    Abandon calculating concurrency for now

--- OpenStack-accounting/sql/nova_get_accounting_data.sql
+++ OpenStack-accounting/sql/nova_get_accounting_data.sql
@@ -2,54 +2,13 @@ CREATE PROCEDURE `get_accounting_data`(IN starttime datetime, IN endtime datetim
 BEGIN
 /*
    Generates accounting data from the Nova database on various resources used
-   A temporary table is set up to run incremental checks to try and establish concurrency stats - This is dropped at the end of the proc
    Some further comments below for the trickier bits
  */
-create table if not exists thecount_concurrency_stats (
-start_time datetime,
-end_time datetime,
-project_id varchar(255),
-flavor_id int ,
-concVMs int,
-concVCPUs int,
-concMem int,
-concSwap int,
-concRoot int,
-concEph int
-);
-set @looptime = starttime;
-while @looptime < endtime do /* loop which iterates through intervals within the time period to get approximate concurrency */
-set @loopend = DATE_ADD(@looptime, INTERVAL 10 MINUTE);
-insert into thecount_concurrency_stats
-SELECT
-    @looptime AS start_time,
-    @loopend AS end_time,
-    i.project_id AS project_id,
-    i.instance_type_id AS flavor_id,
-    COUNT(uuid) AS concVMs,
-    SUM(it.vcpus) AS concVCPUs,
-    SUM(it.memory_mb) AS concMem,
-    SUM(it.swap) AS concSwap,
-    SUM(it.root_gb) AS concRoot,
-    SUM(it.ephemeral_gb) AS concEph
-
-FROM
-    nova.instances i
-        JOIN
-    nova_api.flavors it ON i.instance_type_id = it.id
-WHERE
-    i.created_at <= @loopend
-        AND (i.deleted_at >= @looptime
-        OR ISNULL(i.deleted_at))
-GROUP BY instance_type_id , i.project_id ;
-set @looptime = @loopend;
-end while;
 SELECT
     IFNULL(i.availability_zone, 'nova') AS AvailabilityZone,
     p.name AS Project,
     pp.name AS Department,
     it.name AS Flavor,
-    MAX(ts.concVMs) AS VMs,
     @VMSeconds:=SUM(IF(i.created_at <= starttime /* Captures VMs which were created outside of the period deleted out of the period */
             AND (i.deleted_at >= endtime
             OR ISNULL(i.deleted_at)),
@@ -76,16 +35,6 @@ SELECT
     it.swap AS Swap,
     it.root_gb AS Root_GB,
     it.ephemeral_gb AS Ephemeral_GB,
-    MAX(ts.concMem) AS Max_Conc_Memory_MB,
-    MAX(ts.concVCPUs) AS Max_Conc_VCPU,
-    MAX(ts.concSwap) AS Max_Conc_Swap,
-    MAX(ts.concRoot) AS Max_Conc_Root_GB,
-    MAX(ts.concEph) AS Max_Conc_Ephemeral_GB,
-    MIN(ts.concMem) AS Min_Conc_Memory_MB,
-    MIN(ts.concVCPUs) AS Min_Conc_VCPU,
-    MIN(ts.concSwap) AS Min_Conc_Swap,
-    MIN(ts.concRoot) AS Min_Conc_Root_GB,
-    MIN(ts.concEph) AS Min_Conc_Ephemeral_GB,
     /* The below section extracts metadata to be used for accounting */
     IFNULL((SELECT
                     value
@@ -118,9 +67,6 @@ SELECT
 FROM
     nova.instances i
         JOIN
-    thecount_concurrency_stats ts ON i.instance_type_id = ts.flavor_id
-        AND i.project_id = ts.project_id
-        JOIN
     nova_api.flavors it ON i.instance_type_id = it.id
         JOIN
     keystone.project p ON i.project_id = p.id
@@ -134,5 +80,4 @@ GROUP BY i.availability_zone , p.name , it.name , SUBSTRING(it.name,
     1,
     LOCATE('.', it.name))
 ;
-drop table thecount_concurrency_stats;
 END
