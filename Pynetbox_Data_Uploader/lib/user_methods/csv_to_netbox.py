2adf1996c35425c8a27a24f2ab910d1c723332fa
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2023-10-17 10:54:19 +0100

    ENH: Constructed user method to create netbox devies from csv in CLI

--- Pynetbox_Data_Uploader/lib/user_methods/csv_to_netbox.py
+++ Pynetbox_Data_Uploader/lib/user_methods/csv_to_netbox.py
@@ -1,7 +1,7 @@
-from netbox_api.netbox_create import NetboxCreate
-from netbox_api.netbox_connect import NetboxConnect
-from netbox_api.netbox_check import NetboxCheck
-from utils.csv_to_dict import FormatDict
+from lib.netbox_api.netbox_create import NetboxCreate
+from lib.netbox_api.netbox_connect import NetboxConnect
+from lib.netbox_api.netbox_check import NetboxCheck
+from lib.utils.csv_to_dict import FormatDict
 import argparse
 
 parser = argparse.ArgumentParser(
@@ -13,13 +13,24 @@ parser.add_argument("token", help="Your Netbox Token.")
 parser.add_argument("file_path", help="Your file path to csv files.")
 args = parser.parse_args()
 
-netbox = NetboxConnect(url=args.url, token=args.token)
+netbox = NetboxConnect(url=args.url, token=args.token).api_object()
 device_data = FormatDict(netbox).csv_to_python(args.file_path)
 device_list = FormatDict(netbox).separate_data(device_data)
+exist = NetboxCheck(netbox)
 for device in device_list:
-    exist = NetboxCheck(netbox).check_device_exists(device["device_name"])
-    if exist:
-        raise Exception(f'Device {device["device_name"]} already exists in Netbox.')
-    else:
-        pass
+    device_exist = exist.check_device_exists(device["name"])
+    if device_exist:
+        raise Exception(f'Device {device["name"]} already exists in Netbox.')
+    type_exist = exist.check_device_type_exists(device["device_type"])
+    if not type_exist:
+        raise Exception(f'Type {device["device_type"]} does not exist.')
+
+formatted_list = FormatDict(netbox).iterate_dicts(device_list)
+creation = NetboxCreate(netbox)
+res = creation.create_device(formatted_list)
+print(f'Device created: {res}')
+print("No Errors?")
+
+
+
 
