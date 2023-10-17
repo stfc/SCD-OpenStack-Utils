bff7891643884c0c8ea0f28ee67974d7826f652a
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2023-10-17 11:18:20 +0100

    MAINT: Moved pynetbox logic into a class.
    This keeps the code more organised and readable.

--- Pynetbox_Data_Uploader/lib/user_methods/csv_to_netbox.py
+++ Pynetbox_Data_Uploader/lib/user_methods/csv_to_netbox.py
@@ -2,6 +2,7 @@ from lib.netbox_api.netbox_create import NetboxCreate
 from lib.netbox_api.netbox_connect import NetboxConnect
 from lib.netbox_api.netbox_check import NetboxCheck
 from lib.utils.csv_to_dict import FormatDict
+from typing import List
 import argparse
 
 parser = argparse.ArgumentParser(
@@ -13,43 +14,35 @@ parser.add_argument("token", help="Your Netbox Token.")
 parser.add_argument("file_path", help="Your file path to csv files.")
 args = parser.parse_args()
 
-netbox = NetboxConnect(url=args.url, token=args.token).api_object()
-device_data = FormatDict(netbox).csv_to_python(args.file_path)
-device_list = FormatDict(netbox).separate_data(device_data)
-exist = NetboxCheck(netbox)
-for device in device_list:
-    device_exist = exist.check_device_exists(device["name"])
-    if device_exist:
-        raise Exception(f'Device {device["name"]} already exists in Netbox.')
-    type_exist = exist.check_device_type_exists(device["device_type"])
-    if not type_exist:
-        raise Exception(f'Type {device["device_type"]} does not exist.')
-
-formatted_list = FormatDict(netbox).iterate_dicts(device_list)
-creation = NetboxCreate(netbox)
-res = creation.create_device(formatted_list)
-print(f'Device created: {res}')
-print("No Errors?")
-
 
 class CsvToDict(NetboxConnect):
     def __init__(self, url: str, token: str, file_path: str):
         self.netbox = super(url, token).api_object()
         self.file_path = file_path
+        self.format_dict = FormatDict(self.netbox)
+        self.exist = NetboxCheck(netbox=self.netbox)
+        self.create = NetboxCreate(netbox=self.netbox)
 
-    def read(self):
-        pass
+    def read_csv(self) -> List:
+        device_data = self.format_dict.csv_to_python(self.file_path)
+        device_list = self.format_dict.separate_data(device_data)
+        return device_list
 
-    def parse(self):
-        pass
+    def check_netbox(self, device_list: List) -> bool:
+        for device in device_list:
+            device_exist = self.exist.check_device_exists(device["name"])
+            if device_exist:
+                raise Exception(f'Device {device["name"]} already exists in Netbox.')
+            type_exist = self.exist.check_device_type_exists(device["device_type"])
+            if not type_exist:
+                raise Exception(f'Type {device["device_type"]} does not exist.')
+        return True
 
-    def check(self):
-        pass
+    def convert_data(self,device_list: List) -> List:
+        formatted_list = self.format_dict.iterate_dicts(device_list)
+        return formatted_list
 
-    def convert(self):
-        pass
-
-    def send(self):
-        pass
-
-
+    def send_data(self, device_list: List) -> bool:
+        devices = self.create.create_device(device_list)
+        result = bool(devices)
+        return result
