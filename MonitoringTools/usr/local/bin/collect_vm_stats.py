98a2c7c926bf0bf6da8feb0b4faed278d81703eb
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2024-02-19 11:55:20 +0000

    BUG: fix issue with pagination
    queries were failing on prod because we were asking for too many servers with one call. Implemented a helper function to handle pagination for us

--- MonitoringTools/usr/local/bin/collect_vm_stats.py
+++ MonitoringTools/usr/local/bin/collect_vm_stats.py
@@ -1,5 +1,5 @@
 import sys
-from typing import List
+from typing import List, Dict, Optional
 
 from openstack import connect
 from send_metric_utils import run_scrape, parse_args
@@ -16,6 +16,55 @@ def server_obj_to_len(server_obj) -> int:
     return total_results
 
 
+def run_server_query(
+    conn: connect,
+    filters: Optional[Dict],
+    page_size=1000,
+    call_limit=1000,
+):
+    """
+    Helper method for running server query using pagination - openstacksdk calls can only return a maximum number of
+    values - (set by limit) and to continue getting values we need to run another call pass a "marker" value of the last
+    item seen
+    :param conn: OpenStack cloud connection
+    :param filters: A dictionary of filters to run on the query (server-side)
+    :param page_size: (Default 1000) how many items are returned by single call
+    :param call_limit: (Default 1000) max number of paging iterations.
+        - this is required to mitigate some bugs where successive paging loops back on itself leading to endless calls
+    """
+
+    pagination_filters = {"limit": page_size, "marker": None}
+    if not filters:
+        filters = {}
+
+    new_filters = {**filters, **pagination_filters}
+    query_res = []
+
+    curr_marker = None
+    num_calls = 0
+    while True:
+        if num_calls > call_limit:
+            break
+
+        for i, server in enumerate(conn.compute.servers(details=False, all_projects=True, **new_filters)):
+            query_res.append(server)
+
+            # openstacksdk calls break after going over pagination limit
+            if i == page_size - 1:
+                # restart the for loop with marker set
+                new_filters.update({"marker": server["id"]})
+                break
+
+        # if marker hasn't changed, then has query terminated
+        if new_filters["marker"] == curr_marker:
+            break
+
+        # set marker as current
+        curr_marker = new_filters["marker"]
+        num_calls += 1
+    return query_res
+
+
 def number_servers_total(conn: connect) -> int:
     """
     Query an OpenStack Cloud to find the total number of instances across
@@ -23,7 +72,7 @@ def number_servers_total(conn: connect) -> int:
     :param conn: OpenStack cloud connection
     :returns: Number of VMs in total across the cloud
     """
-    server_obj = conn.compute.servers(details=False, all_projects=True, limit=10000)
+    server_obj = run_server_query(conn, None)
     # get number of items in generator object
     total_instances = server_obj_to_len(server_obj)
     return total_instances
@@ -36,9 +85,7 @@ def number_servers_active(conn: connect) -> int:
     :param conn: OpenStack Cloud Connection
     :returns: Number of active VMs
     """
-    server_obj = conn.compute.servers(
-        details=False, all_projects=True, limit=10000, status="ACTIVE"
-    )
+    server_obj = run_server_query(conn, {"status": "ACTIVE"})
     # get number of items in generator object
     instance_active = server_obj_to_len(server_obj)
     return instance_active
@@ -51,9 +98,7 @@ def number_servers_build(conn: connect) -> int:
     :param conn: OpenStack Cloud Connection
     :returns: Number of VMs in BUILD state
     """
-    server_obj = conn.compute.servers(
-        details=False, all_projects=True, limit=10000, status="BUILD"
-    )
+    server_obj = run_server_query(conn, {"status": "BUILD"})
     # get number of items in generator object
     instance_build = server_obj_to_len(server_obj)
     return instance_build
@@ -66,9 +111,7 @@ def number_servers_error(conn: connect) -> int:
     :param conn: OpenStack Cloud Connection
     :returns: Number of VMs in ERROR state
     """
-    server_obj = conn.compute.servers(
-        details=False, all_projects=True, limit=10000, status="ERROR"
-    )
+    server_obj = run_server_query(conn, {"status": "ERROR"})
     # get number of items in generator object
     instance_err = server_obj_to_len(server_obj)
     return instance_err
@@ -81,29 +124,18 @@ def number_servers_shutoff(conn: connect) -> int:
     :param conn: OpenStack Cloud Connection
     :returns: Number of VMs in SHUTOFF (STOPPED) state
     """
-    server_obj = conn.compute.servers(
-        details=False, all_projects=True, limit=10000, status="SHUTOFF"
-    )
+    server_obj = run_server_query(conn, {"status": "SHUTOFF"})
     # get number of items in generator object
     instance_shutoff = server_obj_to_len(server_obj)
     return instance_shutoff
 
 
-def get_all_server_statuses(cloud_name: str, prod: bool) -> str:
+def get_all_server_statuses(cloud_name: str) -> str:
     """
     Collects the stats for vms and returns a dict
     :param cloud_name: Name of OpenStack cloud to connect to
-    :param prod: Boolean to determine whether Prod or Dev Cloud used
     :return: A comma separated string containing VM states.
     """
-    # raise error if cloud connection not given
-    if not cloud_name:
-        raise ValueError("An OpenStack Connection is required")
-
-    if prod:
-        cloud_env = "Prod"
-    else:
-        cloud_env = "Dev"
 
     # connect to an OpenStack cloud
     conn = connect(cloud=cloud_name)
@@ -114,7 +146,10 @@ def get_all_server_statuses(cloud_name: str, prod: bool) -> str:
     error_vms = number_servers_error(conn)
     shutoff_vms = number_servers_shutoff(conn)
 
-    server_statuses = f"VMStats,instance={cloud_env}totalVM={total_vms}i,activeVM={active_vms}i,buildVM={build_vms}i,errorVM={error_vms}i,shutoffVM={shutoff_vms}i"
+    server_statuses = (f"VMStats,instance={cloud_name.capitalize()} "
+                       f"totalVM={total_vms}i,activeVM={active_vms}i,"
+                       f"buildVM={build_vms}i,errorVM={error_vms}i,"
+                       f"shutoffVM={shutoff_vms}i")
 
     return server_statuses
 
