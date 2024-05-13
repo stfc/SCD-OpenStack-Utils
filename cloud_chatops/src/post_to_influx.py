56a83dbba93b001b6c2fb4864e3ae8b6a81c8fba
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2024-05-13 11:22:05 +0100

    DOC: Explanation of Influx module

--- cloud_chatops/src/post_to_influx.py
+++ cloud_chatops/src/post_to_influx.py
@@ -5,7 +5,13 @@ import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 from time import time_ns
 
-
+"""
+This module is simply for interest and serves no functional purpose.
+It collects the count of open pull requests in each repository and
+sends that data along with a timestamp to an InfluxDB instance.
+Provided this service is run for a long period of time we might see some interesting trends in data.
+For example, new starters in the Cloud or public holidays.
+"""
 class PostDataToInflux:
     """
     This class will post the count of open PRs in each repo to InfluxDB.
