25f4a4d376eb56269e93c000abe4cd2b0cca2593
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2024-05-20 12:46:35 +0100

    ENH: Pushed the timing back an hour
    The timezone should be set on the host and therefore the time will be correct

--- cloud_chatops/async_bot.py
+++ cloud_chatops/async_bot.py
@@ -52,10 +52,10 @@ async def schedule_jobs() -> None:
     schedule.every().monday.at("09:00").do(
         run_pr, mention=True, channel="pull-requests"
     )
-    schedule.every().wednesday.at("10:00").do(run_pr, channel="pull-requests")
-    schedule.every().friday.at("10:00").do(run_pr, channel="pull-requests")
+    schedule.every().wednesday.at("09:00").do(run_pr, channel="pull-requests")
+    schedule.every().friday.at("09:00").do(run_pr, channel="pull-requests")
 
-    schedule.every().day.at("10:00").do(post_data)
+    schedule.every().day.at("09:00").do(post_data)
 
     assert online_notif()
     while True:
