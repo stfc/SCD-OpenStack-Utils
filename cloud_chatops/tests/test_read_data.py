dd761aa47ebd4d651e7a05f60ec89861e6cbb80f
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2024-06-12 11:02:15 +0100

    BUG: Changed test import path

--- cloud_chatops/tests/test_read_data.py
+++ cloud_chatops/tests/test_read_data.py
@@ -1,7 +1,7 @@
 """This test file covers all tests for the read_data module."""
 
 from unittest.mock import patch, mock_open
-from src.read_data import get_token, get_repos, get_user_map, get_maintainer
+from cloud_chatops.read_data import get_token, get_repos, get_user_map, get_maintainer
 
 
 def test_get_token():
