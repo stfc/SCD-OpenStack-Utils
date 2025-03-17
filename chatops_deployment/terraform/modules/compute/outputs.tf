7050006de106ba77b7b5ef393e37e207ab10db20
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2025-03-17 11:53:28 +0000

    ENH: Add terraform code
    Adding infrastrcuture setup in terraform. Creation of networking and
    compute resources. Also associated the floating IP to the load balancer
    as a bastion host

---
+++ chatops_deployment/terraform/modules/compute/outputs.tf
@@ -0,0 +1,11 @@
+output "grafana_host_ips" {
+  value = openstack_compute_instance_v2.grafana.*.access_ip_v4
+}
+
+output "chatops_host_ips" {
+  value = openstack_compute_instance_v2.chatops.*.access_ip_v4
+}
+
+output "prometheus_host_ips" {
+  value = openstack_compute_instance_v2.grafana.*.access_ip_v4
+}
---
+++ chatops_deployment/terraform/modules/networking/outputs.tf
@@ -0,0 +1,19 @@
+output "grafana_secgroup" {
+  value = openstack_networking_secgroup_v2.grafana
+}
+
+output "chatops_secgroup" {
+  value = openstack_networking_secgroup_v2.chatops
+}
+
+output "prometheus_secgroup" {
+  value = openstack_networking_secgroup_v2.prometheus
+}
+
+output "loadbalancer_secgroup" {
+  value = openstack_networking_secgroup_v2.loadbalancer
+}
+
+output "private_network" {
+  value = openstack_networking_network_v2.private_network
+}
