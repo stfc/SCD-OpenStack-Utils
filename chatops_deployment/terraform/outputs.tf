0905622294adf73bb0dd740b54e66f117acd9414
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2025-03-17 11:53:28 +0000

    ENH: Add terraform code
    Adding infrastrcuture setup in terraform. Creation of networking and
    compute resources. Also associated the floating IP to the load balancer
    as a bastion host

---
+++ chatops_deployment/terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "grafana_host_ips" {
+  value = module.compute.grafana_host_ips
+}
+
+output "chatops_host_ips" {
+  value = module.compute.chatops_host_ips
+}
+
+output "prometheus_host_ips" {
+  value = module.compute.prometheus_host_ips
+}
+
+output "loadbalancer_host_ips" {
+  value = var.floating_ip
+}
