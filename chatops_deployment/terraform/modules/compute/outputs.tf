f28bbefda54b9a0194fb58d00abd144b506638c3
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2025-03-31 10:26:55 +0100

    BUG: Terraform outputs wrong data
    Output was sending grafana host IPs to the prometheus variables. Corrected this by changed it to prometheus

--- chatops_deployment/terraform/modules/compute/outputs.tf
+++ chatops_deployment/terraform/modules/compute/outputs.tf
@@ -7,5 +7,5 @@ output "chatops_host_ips" {
 }
 
 output "prometheus_host_ips" {
-  value = openstack_compute_instance_v2.grafana.*.access_ip_v4
+  value = openstack_compute_instance_v2.prometheus.*.access_ip_v4
 }
