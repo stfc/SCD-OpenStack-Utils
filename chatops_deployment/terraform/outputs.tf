6f5fa7c14035d7f951810b88caa851d70837d242
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2025-04-28 09:48:32 +0100

    ENH: Add an OpenStack Cinder volume for Prometheus to mount to
    Using a volume instead of the VM's disk allows us to persist data between deployments of the environment. This is useful for looking at historical data
    TMP: merge add cinder volume

--- chatops_deployment/terraform/outputs.tf
+++ chatops_deployment/terraform/outputs.tf
@@ -21,6 +21,10 @@ output "loadbalancer_private_ip" {
   value = module.compute.loadbalancer_host_ip
 }
 
+output "prometheus_device" {
+  value = module.compute.prometheus_device
+}
+
 output "elasticsearch_device" {
   value = module.compute.elasticsearch_device
 }
