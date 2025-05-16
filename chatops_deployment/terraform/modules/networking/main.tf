00dff9a809f56042ca40b1cfc79005bc4d796fcc
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2025-05-16 09:12:20 +0100

    ENH: Add an Elastic stack VM to Terraform config
    Adding a new VM to the Terraform configuration to host the Elastic "ELK"
    stack. Also, creates security groups for the correct ports needed by
    this service and attaches a volume to the VM to be used as persistent database
    storage.

--- chatops_deployment/terraform/modules/networking/main.tf
+++ chatops_deployment/terraform/modules/networking/main.tf
@@ -90,6 +90,31 @@ resource "openstack_networking_secgroup_rule_v2" "prometheus" {
   security_group_id = openstack_networking_secgroup_v2.prometheus.id
 }
 
+resource "openstack_networking_secgroup_v2" "elasticsearch" {
+  name        = "elasticsearch-${var.deployment}"
+  description = "ElasticSearch host security group."
+}
+
+resource "openstack_networking_secgroup_rule_v2" "logstash" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 5044
+  port_range_max    = 5044
+  remote_ip_prefix  = "192.168.100.0/22"
+  security_group_id = openstack_networking_secgroup_v2.elasticsearch.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "kibana" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 5601
+  port_range_max    = 5601
+  remote_ip_prefix  = "192.168.100.0/22"
+  security_group_id = openstack_networking_secgroup_v2.elasticsearch.id
+}
+
 resource "openstack_networking_secgroup_rule_v2" "alertmanager" {
   direction         = "ingress"
   ethertype         = "IPv4"
