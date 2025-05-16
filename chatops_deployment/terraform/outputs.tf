471bed410af54bc0519a1eee3cd88ff8b89c7655
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2025-05-16 09:12:20 +0100

    ENH: Add an Elastic stack VM to Terraform config
    Adding a new VM to the Terraform configuration to host the Elastic "ELK"
    stack. Also, creates security groups for the correct ports needed by
    this service and attaches a volume to the VM to be used as persistent database
    storage.

--- chatops_deployment/terraform/outputs.tf
+++ chatops_deployment/terraform/outputs.tf
@@ -9,6 +9,9 @@ output "chatops_host_ips" {
 output "prometheus_host_ips" {
   value = module.compute.prometheus_host_ips
 }
+output "elastic_host_ips" {
+  value = module.compute.elastic_host_ips
+}
 
 output "loadbalancer_host_ips" {
   value = var.floating_ip
@@ -16,4 +19,8 @@ output "loadbalancer_host_ips" {
 
 output "loadbalancer_private_ip" {
   value = module.compute.loadbalancer_host_ip
+}
+
+output "elasticsearch_device" {
+  value = module.compute.elasticsearch_device
 }
